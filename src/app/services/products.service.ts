5a4cd8a8d89fe73b29d865a3072ab4660baefce9
BrizziB <boris.brizzi@gmail.com>
2019-03-18 18:58:41 +0100

    -

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Product } from '../Model/product';
